2f0b7fb168f8ae1bf4b4788c6666def6dcd3375c
Zero <zero@cock.li>
2019-08-04 00:46:18 -0400

    Actually prevent Channeling damage inside of factions

--- src/main/java/io/github/animeavi/factionsguard/events/ChannelingEvent.java
+++ src/main/java/io/github/animeavi/factionsguard/events/ChannelingEvent.java
@@ -2,6 +2,8 @@ package io.github.animeavi.factionsguard.events;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockIgniteEvent.IgniteCause;
 import org.bukkit.event.weather.LightningStrikeEvent;
 import org.bukkit.event.weather.LightningStrikeEvent.Cause;
 
@@ -12,6 +14,18 @@ public class ChannelingEvent implements Listener {
     public void onChannelingStrike(LightningStrikeEvent event) {
         if (event.getCause() == Cause.TRIDENT) {
             Faction faction = CommonEvent.getFaction(event.getLightning().getLocation());
+
+            if (CommonEvent.insideOfPlayerFaction(faction)) {
+                event.getLightning().remove();
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockIgnite(BlockIgniteEvent event) {
+        if (event.getCause() == IgniteCause.LIGHTNING) {
+            Faction faction = CommonEvent.getFaction(event.getBlock().getLocation());
 
             if (CommonEvent.insideOfPlayerFaction(faction)) {
                 event.setCancelled(true);
